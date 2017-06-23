f9e8601001e49459d587e10a8bea04b7ae450e9d
htong <htong@scorpius.ece.ncsu.edu>
2017-06-23 18:56:26 -0400

    Plot

--- Process4_main.m
+++ Process4_main.m
@@ -3,28 +3,28 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Figure1
 [ fig, ngenes ] = plotTable( ...
-    'Profies-Genes-ANan-Gene-list-DEG-down-regulated-Not-all-down.csv' );
+    'Profies-Genes-ANan-Gene-list-DEG-down-regulated-Not-all-down.csv', 0 );
 title(sprintf('Profies of Genes shown in Nan down regulated gene list but Not Chang, \n with %d genes',ngenes),...
     'FontSize',14)
 print(fig,'./Figures/fig1','-dpng');
 
 % Figure2
 [ fig, ngenes ] = plotTable( ...
-    'Profiles-ANan-up-regulated.csv' );
+    'Profiles-ANan-up-regulated.csv', 0 );
 title(sprintf('Profies of Genes shown in Nan up regulated gene Analyisis, \n with %d genes',ngenes),...
     'FontSize',14)
 print(fig,'./Figures/fig2','-dpng');
 
 % Figure3
 [ fig, ngenes ] = plotTable( ...
-    'Profiles-ANan-down-regulated.csv' );
+    'Profiles-ANan-down-regulated.csv', 0 );
 title(sprintf('Profies of Genes shown in Nan down regulated gene Analysis, \n with %d genes',ngenes),...
     'FontSize',14)
 print(fig,'./Figures/fig3','-dpng');
 
 % Figure4
 [ fig, ngenes ] = plotTable( ...
-    'Profiles-Genes-ANan-Gene-list-DEG-up-regulated-Not-all-up.csv' );
+    'Profiles-Genes-ANan-Gene-list-DEG-up-regulated-Not-all-up.csv', 0 );
 title(sprintf('Profies of Genes shown in Nan up regulated gene list but Not Chang, \n with %d genes',ngenes),...
     'FontSize',14)
 print(fig,'./Figures/fig4','-dpng');
