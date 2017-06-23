1b688f19a44e45939ba82240b187e8ecde9b08b0
htong <htong@scorpius.ece.ncsu.edu>
2017-06-23 18:45:18 -0400

    plot table

--- plotTable.m
+++ plotTable.m
@@ -1,21 +1,48 @@
-function [ fig, ngene ] = plotTable( csv )
-% Nan
+function [ fig, ngene, expr, agis ] = plotTable( csv, isVariableNames )
+% fig - figure of profiles in csv file
+% ngene - # of genes in the file
+% expr - profiles of genes in RPKM at each time point
+% agis - gene list
+% csv - exel file with 3 replicates for each time point, totally 6 time
+% point
+%       Example of .csv file opened in MS Excell might look as follows:
+%       |    AIG    |  R1T1  |  R2T1  |  R3T1  | ... 
+%       |AT1G01010.1| 1.2691 | 1.7789 | 3.0794 | ...
+%       |    ...    |  ...   |  ...   |   ...  | ...
+%       |AT5G67360.1| 150.93 | 243.19 |   ...  | ...
+%       |    ...    |  ...   |  ...   |   ...  | ... 
+% in this case isVariable set to be 1;
+% isVariableName - 1 if ".csv" file contains Variable names. 0 otherwise.
+%
+if nargin ~= 2
+    fprintf('\ntwo parameter required for the function\n');
+    fprintf(' path of .csv file as the first parameter\n');
+    fprintf(' if the file contains VariableNames as second parameter\n');
+    fprintf(' example:  [ fig, ngene, expr, agis ] = plotTable(''kat-rpkm-expression.csv'', 1)\n\n');
+    return;
+end
+
 %% Read File
-T = readtable(csv,...
+if isVariableNames == 1
+    T = readtable(csv,...
+     'ReadVariableNames',true);
+else
+    T = readtable(csv,...
      'ReadVariableNames',false);
-% summary(T);
+end
+ % summary(T);
 Data = table2array(T(:,2:end));
-name = table2array(T(:,1));
+agis = table2array(T(:,1));
 [ngene,~] = size(Data);
 %% Plot Data Generation
-newData = [];
+expr = [];
 for i = 1:3:21%7 time points; 3 replicates;
-   newData = [newData sum(Data(:,i:i+2),2)];
+   expr = [expr sum(Data(:,i:i+2),2)];
 end
-newData = 1/3*newData;
+expr = 1/3*expr;
 tmp = [];
 for i = 2:7
-   tmp = [tmp log2( newData(:,i)./(newData(:,1)+.01) )];
+   tmp = [tmp log2( expr(:,i)./expr(:,1) )];
 end
 plotData = tmp;
 plotData = [ zeros(size(plotData,1),1) plotData ];
@@ -25,6 +52,11 @@ fig = figure;x = 0 : 1 : 6;
 hold on;axis([0 6 -2 5])
 plot(x, plotData,'Color','[.4,.4,.4]');
 plot(x,mean( plotData),'Color','r','LineWidth',4);
-
+xticks(0:6)
+xticklabels({'0','0.25','0.5','1','4','12','24'})
+title(sprintf( 'plot of expression file\n "%s"', csv))
+xlabel('Ethylene treatment(hrs)');
+ylabel('Expression-log2ratio(reference at 0 hrs)');
+set(gca,'fontsize',14);
 end
 
