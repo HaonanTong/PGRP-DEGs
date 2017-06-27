6d56556c5181d48ddf3ddd5a438e4c34e19ce1f8
htong <htong@scorpius.ece.ncsu.edu>
2017-06-27 18:05:41 -0400

    Huge modidification

--- Process.sh
+++ Process.sh
@@ -2,23 +2,23 @@ echo '=============================================================='
 echo 'Process1: Generating .csv file from txt file in white space...'
 bash Process1_pipeline.sh 
 echo 'End Process1'
-echo '==============================================================/n/n'
+echo '=============================================================='
 
 echo '=============================================================='
 echo 'Process2: deriving DEGs and analyzing...'
 export PATH=$PATH:/Applications/MATLAB_R2017a.app/bin/
 matlab  -r -nodesktop Process2_main
 echo 'End Process2'
-echo '==============================================================/n/n'
+echo '=============================================================='
 
 echo '=============================================================='
 echo 'Process3: Comparing DEGs deriving from Nan v.s. Chang's
 bash Process3_pipeline.sh 
 echo 'End Process3'
-echo '==============================================================/n/n'
+echo '=============================================================='
 
 echo '=============================================================='
 echo 'Process4: Ploting and Concluding...'
 matlab -r -nodesktop Process4_main
 echo 'End Process4'
-echo '==============================================================/n/n'
+echo '=============================================================='
--- Process4_main.m
+++ Process4_main.m
@@ -3,28 +3,28 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Figure1
 [ fig, ngenes ] = f_plotTable( ...
-    'Profies-Genes-ANan-Gene-list-DEG-down-regulated-Not-all-down.csv', 0 );
+    './Result_of_Process3/Profiles-Genes-ANan-Gene-list-DEG-down-regulated-Not-all-down.csv', 1 );
 title(sprintf('Profies of Genes shown in Nan down regulated gene list but Not Chang, \n with %d genes',ngenes),...
     'FontSize',14)
 print(fig,'./Figures/fig1','-dpng');
 
 % Figure2
 [ fig, ngenes ] = f_plotTable( ...
-    'Profiles-ANan-up-regulated.csv', 0 );
+    './Result_of_Process3/Profiles-ANan-up-regulated.csv', 1 );
 title(sprintf('Profies of Genes shown in Nan up regulated gene Analyisis, \n with %d genes',ngenes),...
     'FontSize',14)
 print(fig,'./Figures/fig2','-dpng');
 
 % Figure3
 [ fig, ngenes ] = f_plotTable( ...
-    'Profiles-ANan-down-regulated.csv', 0 );
+    './Result_of_Process3/Profiles-ANan-down-regulated.csv', 1 );
 title(sprintf('Profies of Genes shown in Nan down regulated gene Analysis, \n with %d genes',ngenes),...
     'FontSize',14)
 print(fig,'./Figures/fig3','-dpng');
 
 % Figure4
 [ fig, ngenes ] = f_plotTable( ...
-    'Profiles-Genes-ANan-Gene-list-DEG-up-regulated-Not-all-up.csv', 0 );
+    './Result_of_Process3/Profiles-Genes-ANan-Gene-list-DEG-up-regulated-Not-all-up.csv', 1 );
 title(sprintf('Profies of Genes shown in Nan up regulated gene list but Not Chang, \n with %d genes',ngenes),...
     'FontSize',14)
 print(fig,'./Figures/fig4','-dpng');
